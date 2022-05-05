c3b4ad0ebcc333c9ec9e1f4820908ad473dac3ec
Nikhil874 <95832751+Nikhil874@users.noreply.github.com>
2022-05-05 13:49:46 +0530

    thunk

--- mail-box/src/Redux/store.js
+++ mail-box/src/Redux/store.js
@@ -1,5 +1,5 @@
 import {createStore,combineReducers,applyMiddleware} from "redux";
-
+import thunk from "redux-thunk";
 import { fetchDataReducer } from "../FetchedData/reducer";
 
 const rootReducer=combineReducers(
@@ -9,14 +9,11 @@ const rootReducer=combineReducers(
     }
 )
 //thunk is not updated 1 1/2 years so we better use custom
-const thunk=(store)=>(next)=>(action)=>{
-if(typeof action ==="function") return action(store.dispatch)
+// const thunk=(store)=>(next)=>(action)=>{
+// if(typeof action ==="function") return action(store.dispatch)
 
-next(action)
-}
-// const nikhil=(store)=>(next)=>(action)=>{
-//     console.log("mid2")
-//     next(action)
+// next(action)
 // }
+
 
 export const store=createStore(rootReducer,applyMiddleware(thunk));  //order is very important in applymiddleware
